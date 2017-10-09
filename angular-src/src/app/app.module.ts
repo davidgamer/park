68a788593ad63125a20193e1ef0b5b32260ad5f9
ddsilva <davidhenriquesi@live.com>
2017-10-09 17:26:54 -0300

    Front

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -3,15 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EntradasComponent } from './entradas/entradas.component';
+import {RouterModule, Routes } from '@angular/router';
+import { NovoComponent } from './novo/novo.component';
+import { HomeComponent } from './home/home.component';
+import { CodigosComponent } from './codigos/codigos.component';
 
+
+const appRoutes: Routes =[
+{path:'dashboard',component:DashboardComponent},
+{path:'entradas',component:EntradasComponent},
+{path:'',component:HomeComponent},
+{path:'novo',component:NovoComponent},
+{path:'codigos',component:CodigosComponent}
+
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    DashboardComponent,
+    EntradasComponent,
+    NovoComponent,
+    HomeComponent,
+    CodigosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
